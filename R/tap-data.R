46daf7ffc0d2053930cd68ec75c0a576c1dc6802
wptjd1106 <75157056+wptjd1106@users.noreply.github.com>
2020-11-28 16:13:55 +0900

    first upload

---
+++ R/tap-data.R
@@ -0,0 +1,18 @@
+#' Seoul Tree data
+#'
+#' Data from 'data.seoul.go.kr'
+#'
+#' @docType data
+#'
+#'
+#' @usage data(tap)
+#'
+#' @keywords datasets
+#'
+#'
+#'
+#' @examples
+#' ## Requires font for korean text
+#' data(tap)
+#'
+#'
